cd3ef5fb4105d9632dc00591670d16b082036b01
Sancheopance <sancheopance@gmail.com>
2020-11-20 22:30:31 +0800

    Glopt5

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
     toggleSlide('.delivery-card__link');
     toggleSlide('.delivery-card__back');
 
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" ></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" ></button>',
+      });
+
 
 
 
